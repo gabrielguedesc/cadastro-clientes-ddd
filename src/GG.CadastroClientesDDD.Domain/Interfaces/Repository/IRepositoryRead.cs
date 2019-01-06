27c0ab6bbd8084125c09320288424db522770a6d
Gabriel Guedes Correa <g4brielguedes@gmail.com>
2019-01-06 21:53:16 -0200

    Add: generic repository interfaces.

---
+++ src/GG.CadastroClientesDDD.Domain/Interfaces/Repository/IRepositoryRead.cs
@@ -0,0 +1,15 @@
+﻿using GG.CadastroClientesDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace GG.CadastroClientesDDD.Domain.Interfaces.Repository
+{
+    public interface IRepositoryRead<TEntity> : IDisposable where TEntity : Entity
+    {
+        TEntity GetById(Guid id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> GetPaginate(int skip, int take);
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+    }
+}
---
+++ src/GG.CadastroClientesDDD.Domain/Interfaces/Repository/IRepositoryWrite.cs
@@ -0,0 +1,13 @@
+﻿using GG.CadastroClientesDDD.Domain.Entities;
+using System;
+
+namespace GG.CadastroClientesDDD.Domain.Interfaces.Repository
+{
+    public interface IRepositoryWrite<TEntity> : IDisposable where TEntity : Entity
+    {
+        TEntity Add(TEntity entity);
+        TEntity Update(TEntity entity);
+        void Remove(Guid id);
+        int SaveChanges();
+    }
+}
