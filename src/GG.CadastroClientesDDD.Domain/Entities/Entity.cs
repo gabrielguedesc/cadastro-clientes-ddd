09cc7ee0efa68ba0945c5476a612cb7d765723e2
Gabriel Guedes Correa <g4brielguedes@gmail.com>
2019-01-06 21:43:30 -0200

    Add: generic Entity.

---
+++ src/GG.CadastroClientesDDD.Domain/Entities/Entity.cs
@@ -0,0 +1,24 @@
+﻿using DomainValidation.Validation;
+using System;
+
+namespace GG.CadastroClientesDDD.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public Entity()
+        {
+            Id = new Guid();
+            ValidationResult = new ValidationResult();
+        }
+
+        public Guid Id { get; set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        public abstract bool IsValid();
+
+        public void AddValidationError(string message)
+        {
+            ValidationResult.Add(new ValidationError(message));
+        }
+    }
+}
