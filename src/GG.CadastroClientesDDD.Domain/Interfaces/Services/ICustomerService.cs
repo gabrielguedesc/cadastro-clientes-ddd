454ef7b055c9de2dcc18dc62267abd0dd4928761
Gabriel Guedes Correa <g4brielguedes@gmail.com>
2019-01-07 01:49:55 -0200

    Add: interface ICustomerService, implementation CustomerService and the validation CustomerIsValidForRegistration

---
+++ src/GG.CadastroClientesDDD.Domain/Interfaces/Services/ICustomerService.cs
@@ -0,0 +1,15 @@
+﻿using GG.CadastroClientesDDD.Domain.Entities;
+using System;
+
+namespace GG.CadastroClientesDDD.Domain.Interfaces.Services
+{
+    public interface ICustomerService
+    {
+        Customer Add(Customer customer);
+        Customer GetById(Guid id);
+        Customer GetByCPF(string cpf);
+        Customer GetByEmail(string email);
+        Customer Update(Customer customer);
+        void Remove(Guid id);
+    }
+}
---
+++ src/GG.CadastroClientesDDD.Domain/Services/CustomerService.cs
@@ -0,0 +1,59 @@
+﻿using GG.CadastroClientesDDD.Domain.Entities;
+using GG.CadastroClientesDDD.Domain.Interfaces.Repository;
+using GG.CadastroClientesDDD.Domain.Interfaces.Services;
+using GG.CadastroClientesDDD.Domain.Validations.Customers;
+using System;
+
+namespace GG.CadastroClientesDDD.Domain.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public Customer Add(Customer customer)
+        {
+            if (!customer.IsValid())
+            {
+                return customer;
+            }
+
+            customer.ValidationResult = new CustomerIsValidForRegistration().Validate(customer);
+            if (!customer.ValidationResult.IsValid)
+            {
+                return customer;
+            }
+
+            return _customerRepository.Add(customer);
+        }
+
+        public Customer GetByCPF(string cpf)
+        {
+            return _customerRepository.GetByCPF(cpf);
+        }
+
+        public Customer GetByEmail(string email)
+        {
+            return _customerRepository.GetByEmail(email);
+        }
+
+        public Customer GetById(Guid id)
+        {
+            return _customerRepository.GetById(id);
+        }
+
+        public void Remove(Guid id)
+        {
+            _customerRepository.Remove(id);
+        }
+
+        public Customer Update(Customer customer)
+        {
+            return _customerRepository.Update(customer);
+        }
+    }
+}
