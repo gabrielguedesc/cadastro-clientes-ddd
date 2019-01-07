9d82ebc1438d408a936eb98a041969d10e223bca
Gabriel Guedes Correa <g4brielguedes@gmail.com>
2019-01-07 02:59:17 -0200

    Add: AutoMapper nuget package and bootstrapper

---
+++ src/GG.CadastroClientesDDD.Application/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using GG.CadastroClientesDDD.Application.ViewModels;
+using GG.CadastroClientesDDD.Domain.Entities;
+
+namespace GG.CadastroClientesDDD.Application.AutoMapper
+{
+    public class AutoMapperConfig
+    {
+        public static void RegisterMappings()
+        {
+            Mapper.Initialize(i =>
+            {
+                i.CreateMap<CustomerViewModel, Customer>();
+            });
+        }
+    }
+}
