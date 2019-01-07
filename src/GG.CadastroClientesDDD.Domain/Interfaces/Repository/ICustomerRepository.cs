cd121d8f5cbde39597b2177507dddde3fac71155
Gabriel Guedes Correa <g4brielguedes@gmail.com>
2019-01-07 00:58:38 -0200

    Add: interface ICustomerRepository and implementation CustomerRepository

---
+++ src/GG.CadastroClientesDDD.Domain/Interfaces/Repository/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using GG.CadastroClientesDDD.Domain.Entities;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GG.CadastroClientesDDD.Domain.Interfaces.Repository
+{
+    public interface ICustomerRepository : IRepositoryRead<Customer>, IRepositoryWrite<Customer>
+    {
+        Customer GetByCPF(string cpf);
+        Customer GetByEmail(string email);
+        IEnumerable<Customer> GetAllCustomersActive();
+    }
+}
---
+++ src/GG.CadastroClientesDDD.Infra.Data/Repository/CustomerRepository.cs
@@ -0,0 +1,25 @@
+﻿using GG.CadastroClientesDDD.Domain.Entities;
+using GG.CadastroClientesDDD.Domain.Interfaces.Repository;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GG.CadastroClientesDDD.Infra.Data.Repository
+{
+    public class CustomerRepository : Repository<Customer>, ICustomerRepository
+    {
+        public IEnumerable<Customer> GetAllCustomersActive()
+        {
+            return Find(c => c.Active && !c.Excluded);
+        }
+
+        public Customer GetByCPF(string cpf)
+        {
+            return Find(c => c.Active && !c.Excluded && c.CPF == cpf).FirstOrDefault();
+        }
+
+        public Customer GetByEmail(string email)
+        {
+            return Find(c => c.Active && !c.Excluded && c.Email == email).FirstOrDefault();
+        }
+    }
+}
