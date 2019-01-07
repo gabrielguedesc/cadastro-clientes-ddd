f349d733fcf84a7c3afddd9f1383c3db42a40e23
Gabriel Guedes Correa <g4brielguedes@gmail.com>
2019-01-07 01:05:51 -0200

    Add: SimpleInjector nuget package and SimpleInjectorBootstraper

---
+++ src/GG.CadastroClientesDDD.Infra.CrossCutting.IoC/SimpleInjectorBootstraper.cs
@@ -0,0 +1,15 @@
+﻿using GG.CadastroClientesDDD.Domain.Interfaces.Repository;
+using GG.CadastroClientesDDD.Infra.Data.Repository;
+using SimpleInjector;
+
+namespace GG.CadastroClientesDDD.Infra.CrossCutting.IoC
+{
+    public class SimpleInjectorBootstraper
+    {
+        public static void Register(Container container)
+        {
+            //Data
+            container.Register<ICustomerRepository, CustomerRepository>(Lifestyle.Scoped);
+        }
+    }
+}
