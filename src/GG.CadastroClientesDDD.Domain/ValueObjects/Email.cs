122393dd60134e04c797baee876ddc6d9de3ec75
Gabriel Guedes Correa <g4brielguedes@gmail.com>
2019-01-06 23:07:41 -0200

    Add: value objects CPF and Email

---
+++ src/GG.CadastroClientesDDD.Domain/ValueObjects/Email.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace GG.CadastroClientesDDD.Domain.ValueObjects
+{
+    class Email
+    {
+        public static bool Validate(string email)
+        {
+            return Regex.IsMatch(email, @"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", RegexOptions.IgnoreCase);
+        }
+    }
+}
